254f80baa5ce1298af94048fadd199691283f34f
shivani <patelshivani3008@gmail.com>
2023-03-29 17:56:11 +0530

    Hii

---
+++ test2.java
@@ -0,0 +1,10 @@
+class Test{
+    private int id;
+    private String name;
+    private int age;
+}
+class Main{
+    public static void main(String arg[]){
+        System.out.println("Hello java");
+    }
+}
